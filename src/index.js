b0ac543b8de3bb2cfa529a863a565d6f590cd6ba
im36-123 <im36.1230@gmail.com>
2018-09-28 20:00:39 +0900

    update: normalize.css 追加

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import IndecisionApp from "./components/IndecisionApp";
+import '../node_modules/normalize.css/normalize.css'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("root"));
